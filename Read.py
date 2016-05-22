0b4e118a8e3e8a3bdcac8454105d16af771bf031
Philipp <github@philippeisen.de>
2016-05-22 12:12:24 +0200

    if put

--- Read.py
+++ Read.py
@@ -12,7 +12,7 @@ continue_reading = True
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal, frame):
     global continue_reading
-    print("Ctrl+C captured, ending read.")
+    print "Ctrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
 
@@ -24,39 +24,47 @@ signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
 # Welcome message
-# print "Welcome to the MFRC522 data read example"
-# print "Press Ctrl-C to stop."
+print("Welcome to the MFRC522 data read example")
+print("Press Ctrl-C to stop.")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
 
     # Scan for cards    
-    # (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
-    # if status == MIFAREReader.MI_OK:
-    # print "Card detected"
+    if status == MIFAREReader.MI_OK:
+        print("Card detected")
 
     # Get the UID of the card
     (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
+
         # Print UID
-        print(str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
+
+        print("Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
+        print('unrolled uid:')
+        print(str(*uid))
+        if (str(*uid)) == '456789231':
+            r = requests.post('127.0.0.1', data='1')
+        if (str(*uid)) == '34534543':
+            r = requests.post('127.0.0.1', data='2')
 
         # This is the default key for authentication
-        # key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
 
         # Select the scanned tag
-        # MIFAREReader.MFRC522_SelectTag(uid)
+        MIFAREReader.MFRC522_SelectTag(uid)
 
         # Authenticate
-        # status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
 
-        # # Check if authenticated
-        # if status == MIFAREReader.MI_OK:
-        #     MIFAREReader.MFRC522_Read(8)
-        #     MIFAREReader.MFRC522_StopCrypto1()
-        # else:
-        #     print "Authentication error"
+        # Check if authenticated
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print "Authentication error"
