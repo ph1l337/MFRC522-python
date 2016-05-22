e51e2119cb69bc1b82365a7ff015f93134683554
Philipp <github@philippeisen.de>
2016-05-22 12:20:03 +0200

    new changes

--- Read.py
+++ Read.py
@@ -47,10 +47,9 @@ while continue_reading:
 
         print("Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
         print('unrolled uid:')
-        print(','.join(str(uid)))
-        if (','.join(str(uid))) == '456789231':
+        if str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]) == '456789231':
             r = requests.post('127.0.0.1', data='1')
-        if (','.join(str(uid))) == '34534543':
+        if str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]) == '34534543':
             r = requests.post('127.0.0.1', data='2')
 
         # This is the default key for authentication
